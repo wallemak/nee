d86a5753ce0ff1b61f5ab1948616a4664ebd2761
nee <nee@taolin.com>
2019-05-23 17:11:19 +0800

    添加了图标

--- home/js/common.js
+++ home/js/common.js
@@ -1,9 +1,26 @@
 (function()
 {
-	$.get("http://192.168.131.1:8088/home/nav",{},function(data,status){
+	$.get("http://192.168.131.1:8090/all_nav",{},function(data,status){
 		// console.log(data);
-	},'jsonp')
-	
-		
+		new Vue({ el:"#mynav",data:{nav:data} });
+		// new Vue({ el:'.sin_btn',data:{list:data.class_list} });
+
+	},'jsonp');
+	$(".nav_ico").click(function(){
+		var name = $(".nav_ico img").eq(0).attr("name");
+		switch(true)
+		{
+			case name=='list':
+			$(".nav_ico img").eq(0).attr("src","images/clos.png");
+			$(".nav_ico img").eq(0).attr("name","clos");
+			$(".middle").css('display','inline-block');
+			break;
+			case name =='clos':
+			$(".nav_ico img").eq(0).attr("src","images/list.png");
+			$(".nav_ico img").eq(0).attr("name","list");
+			$(".middle").css('display','none');
+			break;
+		}
+	});
 	
 })();
